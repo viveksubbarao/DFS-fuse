be0b5c8ac3efdc2ac78a8b01bde3b5690ce81401
Vivek S <vsubbar@ncsu.edu>
2016-04-02 15:14:38 -0400

    Removed directories "DFS server", "DFS-Python" and "DFS server client by rakesh". Renamed file myfuse.py to client.py. Renamed file_attribute_server.py to server.py

--- client.py
+++ client.py
@@ -7,9 +7,14 @@ from __future__ import with_statement
 import os
 import sys
 import errno
+import subprocess
+import socket
 
 from fuse import FUSE, FuseOSError, Operations
 
+HOST = ''    # The remote host
+PORT = 50007 # The same port as used by the server
+sock = -1
 
 class Passthrough(Operations):
     def __init__(self, root):
@@ -28,25 +33,30 @@ class Passthrough(Operations):
     # ==================
 
     def access(self, path, mode):
+        print("access")
         full_path = self._full_path(path)
         if not os.access(full_path, mode):
             raise FuseOSError(errno.EACCES)
 
     def chmod(self, path, mode):
+        print("chmod")
         full_path = self._full_path(path)
         return os.chmod(full_path, mode)
 
     def chown(self, path, uid, gid):
+        print("chown")
         full_path = self._full_path(path)
         return os.chown(full_path, uid, gid)
 
     def getattr(self, path, fh=None):
+        print("getattr")
         full_path = self._full_path(path)
         st = os.lstat(full_path)
         return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
                      'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
 
     def readdir(self, path, fh):
+        print("readdir");
         full_path = self._full_path(path)
 
         dirents = ['.', '..']
@@ -56,6 +66,7 @@ class Passthrough(Operations):
             yield r
 
     def readlink(self, path):
+        print("readlink")
         pathname = os.readlink(self._full_path(path))
         if pathname.startswith("/"):
             # Path name is absolute, sanitize it.
@@ -64,16 +75,20 @@ class Passthrough(Operations):
             return pathname
 
     def mknod(self, path, mode, dev):
+        print("mknod")
         return os.mknod(self._full_path(path), mode, dev)
 
     def rmdir(self, path):
+        print("rmdir")
         full_path = self._full_path(path)
         return os.rmdir(full_path)
 
     def mkdir(self, path, mode):
+        print("mkdir")
         return os.mkdir(self._full_path(path), mode)
 
     def statfs(self, path):
+        print("statfs")
         full_path = self._full_path(path)
         stv = os.statvfs(full_path)
         return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
@@ -81,55 +96,68 @@ class Passthrough(Operations):
             'f_frsize', 'f_namemax'))
 
     def unlink(self, path):
+        print("unlink")
         return os.unlink(self._full_path(path))
 
     def symlink(self, name, target):
+        print("symlink")
         return os.symlink(name, self._full_path(target))
 
     def rename(self, old, new):
+        print("rename")
         return os.rename(self._full_path(old), self._full_path(new))
 
     def link(self, target, name):
+        print("link")
         return os.link(self._full_path(target), self._full_path(name))
 
     def utimens(self, path, times=None):
+        print("utimes")
         return os.utime(self._full_path(path), times)
 
     # File methods
     # ============
 
     def open(self, path, flags):
+        print("open")
         full_path = self._full_path(path)
         return os.open(full_path, flags)
 
     def create(self, path, mode, fi=None):
+        print("create")
         full_path = self._full_path(path)
         return os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
 
     def read(self, path, length, offset, fh):
+        print("read")
         os.lseek(fh, offset, os.SEEK_SET)
         return os.read(fh, length)
 
     def write(self, path, buf, offset, fh):
+        print("write")
         os.lseek(fh, offset, os.SEEK_SET)
         return os.write(fh, buf)
 
     def truncate(self, path, length, fh=None):
+        print("truncate")
         full_path = self._full_path(path)
         with open(full_path, 'r+') as f:
             f.truncate(length)
 
     def flush(self, path, fh):
+        print("flush")
         return os.fsync(fh)
 
     def release(self, path, fh):
+        print("release")
         return os.close(fh)
 
     def fsync(self, path, fdatasync, fh):
+        print("fsync")
         return self.flush(path, fh)
 
-
 def main(mountpoint, root):
+    global sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     FUSE(Passthrough(root), mountpoint, nothreads=True, foreground=True)
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -2,17 +2,49 @@
 import socket
 import os
 
+# Filesystem methods
+DFS_ACCESS = 1
+DFS_CHMOD = 2
+DFS_CHOWN = 3
+DFS_GETATTR = 4
+DFS_READDIR = 5
+DFS_READLINK = 6
+DFS_MKNOD = 7
+DFS_RMDIR = 8
+DFS_MKDIR = 9
+DFS_STATFS = 10
+DFS_UNLINK = 11
+DFS_SYMLINK = 12
+DFS_RENAME = 13
+DFS_LINK = 14
+DFS_UTIMES = 15
+
+# File methods
+DFS_OPEN = 16
+DFS_CREATE = 17
+DFS_READ = 18
+DFS_WRITE = 19
+DFS_TRUNCATE = 20
+DFS_FLUSH = 21
+DFS_RELEASE = 22
+DFS_FSYNC = 23
+
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind((HOST, PORT))
-#s.listen(1)
-#conn, addr = s.accept()
-#print 'Connected by', 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
 
-#f = open("cats.png","wb")
+while 1:
+    conn, addr = s.accept()
+    print 'Connected by ', addr
+    data = conn.recv(64)
+    print data, len(data)
+    conn.close()
 
+s.close()
 
+''''
 while 1:
 	print 
 	print 'Enter 1 for list of file names'
@@ -36,11 +68,11 @@ while 1:
 		break
 	#data = conn.recv(100000)
 	
-
 #if not data: break
 #conn.sendall(data)
 
 #print 'file received'
 
-f.close()
+#f.close()
 conn.close()
+'''
